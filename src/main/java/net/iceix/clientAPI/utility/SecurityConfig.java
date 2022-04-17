dab6c9d80a7c82a37d9c0ea9575d2b3499268ffd
Tom Glaser <tfglaser@gmail.com>
2022-04-17 05:37:45 -0500

    Added OPA Security

---
+++ src/main/java/net/iceix/clientAPI/utility/SecurityConfig.java
@@ -0,0 +1,15 @@
+package net.iceix.clientAPI.utility;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+}
+
+}
