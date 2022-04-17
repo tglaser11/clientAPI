8ff0c1c8a5ee7d4950e76197d6c340f984123444
Tom Glaser <tfglaser@gmail.com>
2022-04-17 05:47:26 -0500

    more data

--- src/main/java/net/iceix/clientAPI/utility/DataLoader.java
+++ src/main/java/net/iceix/clientAPI/utility/DataLoader.java
@@ -17,12 +17,12 @@ public class DataLoader {
     private void loadData() {
         repository.deleteAll();
 
-        repository.save(new Client( "1234","Tom","Glaser"));
-        repository.save(new Client( "1000","Kaitlin","Glaser"));
-        repository.save(new Client( "1001","Jim","Jones"));
-        repository.save(new Client( "1002","Mary","Smith"));
-        repository.save(new Client( "1003","Harry","Hughes"));
-        repository.save(new Client( "1004","Amanda","Sinclair"));
-        repository.save(new Client( "1005","Gina","Smith"));
+        repository.save(new Client( "1234","Aldous","Huxley"));
+        repository.save(new Client( "1000","Virginia","Woolf"));
+        repository.save(new Client( "1001","Charles","Dickens"));
+        repository.save(new Client( "1002","Harper","Lee"));
+        repository.save(new Client( "1003","Leo","Tolstoy"));
+        repository.save(new Client( "1004","Jane","Austien"));
+        repository.save(new Client( "1005","JK","Rowling"));
     }
 }
